36f6b700dcb61b0adc0ea66fd837a0e3fe5f6810
geno-whirl <tlungens@gmail.com>
2016-01-29 15:43:15 -0300

    Problemita...

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
-  
-  root 'pages#home'
 
-  get 'beneficios' => 'pages#beneficios'
-
-  get 'productos' => 'pages#productos'
-
-  get 'comprar' => 'pages#comprar'
-
-  get 'about' => 'pages#about'
-
-  get 'contacto' => 'pages#contacto'
+  root 'Toti#home'
   
 end
